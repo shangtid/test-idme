04b42dba9293be2896ad2e463124865924ebc328
shangtid <shangtiwright@gmail.com>
2016-08-18 16:41:27 -0400

    Add files via upload

--- id.py
+++ id.py
@@ -1,2 +1,2 @@
-src="https://s3.amazonaws.com/idme/developer/idme-buttons-2.0.1/assets/js/idme-modal.min.js"
-type="text/javascript"
+"idme-btn-container><span class=idme-btn-affinity>[AFFINITY GROUP] receives [OFFER] off with ID.me</spa<a href=# class=idme-btn<img src=https://s3.amazonaws.com/idme/developer/idme-buttons-2.0.1/assets/img/btn-alt-Responder.png border=0 alt=Verify with ID.me Responder ID class=idme-btn-primary-sm-Responder></a><a href= class=idme-btn>
+"https://s3.amazonaws.com/idme/developer/idme-buttons-2.0.1/assets/img/btn-alt-Government.png" "0" "Verify with ID.me Government ID" "idme-btn-primary-sm-Government"
